0cb4c104cd54e82c1890649e060326221f9728ca
Pratik-kashte12 <kashtepratik76@gmail.com>
2026-07-26 15:26:03 +0530

    connect frontend to live backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://skill-exchange-backend-x1rt.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
